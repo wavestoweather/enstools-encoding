81ca4569305e708d5dc5c5eb32546b69f2e8e33a
Oriol Tintó <oriol.tinto@lmu.de>
2022-09-08 14:08:17 +0200

    Make availability checks visible from the module level.

--- enstools/compression/__init__.py
+++ enstools/compression/__init__.py
@@ -1,2 +1,4 @@
-from .definitions import Compressors, CompressionModes
-from .compression_specification import FilterEncodingForXarray, FilterEncodingForH5py
+from .definitions import Compressors, CompressionModes, compressor_aliases, compression_mode_aliases
+from .encodings import FilterEncodingForXarray, FilterEncodingForH5py
+from .compressors.availability_checks import check_dataset_filters_availability, check_filters_availability, \
+    check_sz_availability, check_zfp_availability, check_blosc_availability, check_libpressio_availability
