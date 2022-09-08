ad97b289263f3d5d211957dba6d0e2b6ddbbb111
Oriol Tintó <oriol.tinto@lmu.de>
2022-09-08 15:49:21 +0200

    Add comment to

--- enstools/compression/compressors/no_compressor.py
+++ enstools/compression/compressors/no_compressor.py
@@ -1,7 +1,15 @@
+"""
+In order to keep a structure,
+we define here a NoCompression class to be used whenever we don't want to compress part of a dataset.
+"""
+
 from .compressor_class import Compressor
 
 
 class NoCompression(Compressor):
+    """
+    Compressor subclass for when we don't want any compression.
+    """
     filter_id = None
     filter_options = None
     chunksizes = None
