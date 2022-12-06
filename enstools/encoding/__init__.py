7e491f82db5b82c1469bd4ff304fac27137d05d2
Oriol Tintó <oriol.tinto@lmu.de>
2022-12-06 15:25:40 +0100

    Remove __version__.

--- enstools/encoding/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-# to convert enstools into a namespace package, the version is now listed here and not in the level above
-import pkg_resources  # part of setuptools
-version = pkg_resources.require("enstools-encoding")[0].version
-__version__ = version
-"""
-
-# FIXME: There should be a proper way of doing this. Meanwhile I just keep it without a __version__ variable.
