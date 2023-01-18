63ad20ec75ca2a60f12f995d5d94127ed1294f2e
Oriol Tintó <oriol.tinto@lmu.de>
2023-01-18 11:50:40 +0100

    Simplify error classes.

--- enstools/encoding/errors.py
+++ enstools/encoding/errors.py
@@ -2,25 +2,5 @@ class EnstoolsCompressionError(Exception):
     ...
 
 
-class WrongCompressionSpecificationError(EnstoolsCompressionError):
-    ...
-
-
-class WrongCompressionModeError(EnstoolsCompressionError):
-    ...
-
-
-class WrongCompressorError(EnstoolsCompressionError):
-    ...
-
-
-class WrongParameterError(EnstoolsCompressionError):
-    ...
-
-
-class CompressionStringFormatException(EnstoolsCompressionError):
-    ...
-
-
-class FilterNotAvailable(EnstoolsCompressionError):
+class InvalidCompressionSpecification(EnstoolsCompressionError):
     ...
