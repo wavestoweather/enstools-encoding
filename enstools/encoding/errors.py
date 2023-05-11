98d69771608fc7044ebb7a173004a3ec4dffad04
Oriol Tintó <oriol.tinto@lmu.de>
2023-05-11 16:57:59 +0200

    Clean code following pylint comments.

--- enstools/encoding/errors.py
+++ enstools/encoding/errors.py
@@ -1,6 +1,16 @@
+"""
+This module provides exceptions related to compression in Enstools.
+
+"""
+
+
 class EnstoolsCompressionError(Exception):
-    ...
+    """
+    Base exception class for Enstools compression-related errors.
+    """
 
 
 class InvalidCompressionSpecification(EnstoolsCompressionError):
-    ...
+    """
+    Exception raised for an invalid compression specification.
+    """
