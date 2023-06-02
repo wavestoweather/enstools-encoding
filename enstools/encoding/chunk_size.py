366192725938e987add56491fc5c822ddede945b
Oriol Tintó <oriol.tinto@lmu.de>
2023-06-02 16:02:59 +0200

    Adding analysis_chunk_size

--- enstools/encoding/chunk_size.py
+++ enstools/encoding/chunk_size.py
@@ -1,6 +1,10 @@
 # Using a module file to store the desired chunk size.
+
+# Chunk size used to save files to disk
 chunk_size = "10MB"
 
+# Chunk size used for the analysis
+analysis_chunk_size = "100KB"
 def change_chunk_size(new_chunk_size: str):
     global chunk_size
     chunk_size = new_chunk_size
